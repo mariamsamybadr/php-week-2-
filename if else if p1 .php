c22ba27bf466791e048aa1c55c294a4d0ef24e25
Mariam Samy Badr <114897239+mariamsamybadr@users.noreply.github.com>
2023-04-15 01:33:55 +0200

    Add files via upload

---
+++ if else if p1 .php
@@ -0,0 +1,18 @@
+<?php
+
+  
+// it will be checked and run by order 
+
+  if (10 > 5) {
+
+    echo "First Condition";
+
+  } else if (10 > 6) {
+
+    echo "Second Condition";
+
+  } else {
+
+    echo "No";
+
+  }
